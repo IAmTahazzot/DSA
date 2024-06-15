c1f0dcb4e29ee7c35e493d99fdbaf6846714db13
Tahazzot <iamtahazzot@gmail.com>
2024-06-15 18:14:06 +0600

    I hate regular expression but I must love it, F***

---
+++ challenges/8. CamelCase 4/solution.py
@@ -0,0 +1,85 @@
+"""
+Intro:
+
+Camel Case is a naming style common in many programming languages. In Java, method and variable names typically start with a lowercase letter, with all subsequent words starting with a capital letter (example: startThread). Names of classes follow the same pattern, except that they start with a capital letter (example: BlueCar).
+
+Your task is to write a program that creates or splits Camel Case variable, method, and class names.
+
+Input Format:
+
+- Each line of the input file will begin with an operation (S or C) followed by a semi-colon followed by M, C, or V followed by a semi-colon followed by the words you'll need to operate on.
+- The operation will either be S (split) or C (combine)
+- M indicates method, C indicates class, and V indicates variable
+
+In the case of a split operation, the words will be a camel case method, class or variable name that you need to split into a space-delimited list of words starting with a lowercase letter.
+
+In the case of a combine operation, the words will be a space-delimited list of words starting with lowercase letters that you need to combine into the appropriate camel case String. Methods should end with an empty set of parentheses to differentiate them from variable names.
+Output Format
+
+For each input line, your program should print either the space-delimited list of words (in the case of a split operation) or the appropriate camel case string (in the case of a combine operation).
+
+Sample Input:
+
+S;M;plasticCup()
+C;V;mobile phone
+C;C;coffee machine
+S;C;LargeSoftwareBook
+C;M;white sheet of paper
+S;V;pictureFrame
+
+Sample Output:
+
+plastic cup
+mobilePhone
+CoffeeMachine
+large software book
+whiteSheetOfPaper()
+picture frame
+
+Explanation:
+
+Use Scanner to read in all information as if it were coming from the keyboard.
+Print all information to the console using standard output (System.out.print() or System.out.println()).
+Outputs must be exact (exact spaces and casing).
+"""
+
+import re
+
+
+def transformCamelCase(input: str) -> str:
+    action, data_type, word = input.split(";")
+
+    word = word.removesuffix("()")
+    word_parts = re.findall(r"[A-Z][^A-Z\s]*|[^A-Z\s]+", word)
+
+    if action == "S":
+        # split
+        return " ".join(part.lower() for part in word_parts)
+    else:
+        # combine
+        combined_word = "".join(
+            (
+                (
+                    part[0].upper()
+                    if data_type == "C" and index == 0
+                    else (
+                        part[0].lower()
+                        if data_type != "C" and index == 0
+                        else part[0].upper()
+                    )
+                )
+                + part[1:]
+            )
+            for index, part in enumerate(word_parts)
+        )
+
+        return combined_word + "()" if data_type == "M" else combined_word
+
+
+if __name__ == "__main__":
+    print(transformCamelCase("S;M;plasticCup()"))
+    print(transformCamelCase("C;V;mobile phone"))
+    print(transformCamelCase("C;C;coffee machine"))
+    print(transformCamelCase("S;C;LargeSoftwareBook"))
+    print(transformCamelCase("C;M;white sheet of paper"))
+    print(transformCamelCase("S;V;pictureFrame"))
